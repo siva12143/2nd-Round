6a322470ed9c958bcc96cfe13509070b4fb1c89d
siva12143 <siva121435@gmail.com>
2025-03-14 17:44:04 +0530

    Update

--- removeDuplicateArray.js
+++ removeDuplicateArray.js
@@ -3,8 +3,21 @@ const removeElement = (val) =>{
     for(let i = 0; i<newList.length-1; i++){
         for(let  j = i+1;j<newList.length; j++){
             if(newList[i] == newList[j]){
-                
+                for(let k = j; k<newList.length -1; k++){
+                    let val = newList[k];
+                    newList[k] = newList[k+1];
+                    newList[k+1] = val;
+                }
+                newList.length -=1;
+            }
+            if(newList[i] > newList[j]){
+                const val = newList[i];
+                newList[i] = newList[j];
+                newList[j] = val;
             }
         }
     }
+    return newList;
 }
+const arr= [1,1,2,3,3,6,7,9,7,9,8,8,4,5,5,6];
+console.log(removeElement(arr));
